b47287ab829aa848c5c28d330a5c108d87cdaf54
Dev Keshwani <dev.keshwani345@gmail.com>
2024-12-02 00:48:47 +0530

    feat: day-1 solution added, not optimised

--- solutions/day-01.py
+++ solutions/day-01.py
@@ -1,16 +1,34 @@
 """Solution for Advent of Code - Day 1"""
 
 def solve_part_1(input_data):
-    """
-    Solve Part 1 of the challenge for this day.
-    """
+    lst_left=[]
+    lst_right=[]
+    for i in input_data.split("\n"):
+        split=i.split("   ")
+        lst_left.append(int(split[0]))
+        lst_right.append(int(split[1]))
+            
+    lst_left.sort()
+    lst_right.sort()
+
+    sum=0
+    for i in range(len(lst_left)-1):
+        sum+=abs(lst_right[i]-lst_left[i])
     
+    return sum
 
 def solve_part_2(input_data):
-    """
-    Solve Part 2 of the challenge for this day.
-    """
-    # TODO: Implement Part 2 solution
-    return None
+    lst_left=[]
+    lst_right=[]
+    for i in input_data.split("\n"):
+        split=i.split("   ")
+        lst_left.append(int(split[0]))
+        lst_right.append(int(split[1]))
+   
+    sum=0
+    for i in range(len(lst_left)-1):
+        num=lst_left[i]
+        cnt=lst_right.count(num)
+        sum+=num*cnt
 
-# Optional: Add helper functions if needed
+    return sum
